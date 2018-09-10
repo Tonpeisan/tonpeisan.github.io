b2b4a3de06e0a90b68c95982db56e8f646be61e6
Tonpeisan <16087098+Tonpeisan@users.noreply.github.com>
2018-09-11 01:38:37 +0900

    パースして曲名、得点、レベルを抽出するところまで

--- bookmarklet.js
+++ bookmarklet.js
@@ -18,8 +18,15 @@
         return new Promise(function(resolve){
             _ajax("https://ongeki-net.com/ongeki-mobile/record/musicGenre/search/", type = "get", { genre: 99, diff: 3 }).then(function(data){
             let parser = new DOMParser();
-            var doc = parser.parseFromString(data, "text/html");
-            console.log(doc)
+            let doc = parser.parseFromString(data, "text/html");
+            let elements = doc.getElementsByClassName('basic_btn master_score_back m_10 p_5 t_l');
+            for (let index=0; index < elements.length; index++){
+                let music = elements[index];
+                let name = music.getElementsByClassName('music_label p_5 break')[0].textContent;
+                let level = music.getElementsByClassName('score_level t_c')[0].textContent;
+                let techscore = music.getElementsByClassName('score_value master_score_value')[2].textContent;
+                console.log(name, level, techscore);
+            }
             resolve(doc);
             })
         });    
@@ -29,8 +36,7 @@
         return new Promise(function (resolve) {
             _ajax("https://ongeki-net.com/ongeki-mobile/home/playerDataDetail/", type = "get", {}).then(function (data) {
                 let parser = new DOMParser();
-                var doc = parser.parseFromString(data, "text/html");
-                console.log(doc)
+                let doc = parser.parseFromString(data, "text/html");
                 resolve(doc);
             })
         });    
